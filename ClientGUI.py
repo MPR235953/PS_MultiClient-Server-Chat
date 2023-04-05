10c8c5374e84970fd863074fa2162069a867c216
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-04-05 13:24:24 +0200

    feat: chenageable GUI view depends on connection status

--- ClientGUI.py
+++ ClientGUI.py
@@ -1,18 +1,22 @@
 from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import QMainWindow, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QApplication
 from PyQt5 import QtCore, QtWidgets
 import sys
 
-class GUI(QMainWindow):
+from Client import Client
+
+class ClientGUI(QMainWindow):
 
     def __init__(self):
 
         super().__init__()
-        self.__width = 600
+        self.__width = 700
         self.__height = 400
         self.__perc = (0.1, 0.8)
 
         self.__setup_GUI()
+
+        self.client = Client()
 
     def __setup_GUI(self):
 
@@ -24,6 +28,8 @@ class GUI(QMainWindow):
         self.__set_receive()
         self.__set_status()
 
+        self.__connection_GUI_setter(conn_status=False)
+
     def __set_IP(self):
 
         self.lbIP = QtWidgets.QLabel(self)
@@ -34,6 +40,8 @@ class GUI(QMainWindow):
         self.teIP = QtWidgets.QTextEdit(self)
         self.teIP.setGeometry(QtCore.QRect(60, 40, 100, 30))
         self.teIP.setObjectName("teIP")
+        self.teIP.setPlaceholderText("127.0.0.1")
+        self.teIP.setText("127.0.0.1")  # TODO: change it, only for debug
 
     def __set_port(self):
 
@@ -45,6 +53,8 @@ class GUI(QMainWindow):
         self.tePort = QtWidgets.QTextEdit(self)
         self.tePort.setGeometry(QtCore.QRect(60, 95, 100, 30))
         self.tePort.setObjectName("tePort")
+        self.tePort.setPlaceholderText("5000")
+        self.tePort.setText("5000") # TODO: change it, only for debug
 
     def __set_conn(self):
 
@@ -54,8 +64,8 @@ class GUI(QMainWindow):
         self.btnConn = QtWidgets.QPushButton(self)
         self.btnConn.setGeometry(QtCore.QRect(60, 130, 100, 25))
         self.btnConn.setObjectName("btnConn")
-        self.btnConn.setText("SUBMIT")
-        self.btnConn.clicked.connect(self.__show_popup_fail)
+        self.btnConn.setText("Connect")
+        self.btnConn.clicked.connect(self.__connect)
 
     def __set_send(self):
 
@@ -63,16 +73,20 @@ class GUI(QMainWindow):
         self.lbSend.setGeometry(QtCore.QRect(60, 200, 100, 15))
         self.lbSend.setObjectName("lbSend")
         self.lbSend.setText("Text to send")
+        self.lbSend.setDisabled(True)
 
         self.teSend = QtWidgets.QTextEdit(self)
         self.teSend.setGeometry(QtCore.QRect(60, 220, 200, 100))
         self.teSend.setObjectName("teSend")
+        self.teSend.setDisabled(True)
 
         self.btnSend = QtWidgets.QPushButton(self)
         self.btnSend.setGeometry(QtCore.QRect(60, 325, 100, 25))
         self.btnSend.setObjectName("btnSend")
         self.btnSend.setText("SEND")
+        self.btnSend.setCheckable(True)
         self.btnSend.clicked.connect(self.__show_popup_fail)
+        self.btnSend.setDisabled(True)
 
     def __set_receive(self):
 
@@ -80,10 +94,13 @@ class GUI(QMainWindow):
         self.lbReceive.setGeometry(QtCore.QRect(320, 200, 100, 15))
         self.lbReceive.setObjectName("lbReceive")
         self.lbReceive.setText("Received text")
+        self.lbReceive.setDisabled(True)
 
         self.teReceive = QtWidgets.QTextEdit(self)
         self.teReceive.setGeometry(QtCore.QRect(320, 220, 200, 100))
         self.teReceive.setObjectName("teReceive")
+        self.teReceive.setReadOnly(True)
+        self.teReceive.setDisabled(True)
 
     def __set_status(self):
 
@@ -93,19 +110,33 @@ class GUI(QMainWindow):
         self.lbConnState.setText("State")
 
         self.teConnState = QtWidgets.QTextEdit(self)
-        self.teConnState.setGeometry(QtCore.QRect(int(0.8 * self.__width), int(0.1 * self.__height), 100, 30))
+        self.teConnState.setGeometry(QtCore.QRect(int(0.8 * self.__width), int(0.1 * self.__height), 120, 30))
         self.teConnState.setObjectName("teConnState")
-        self.teConnState.setEnabled(False)
-        self.teConnState.setTextColor(QColor(0, 0, 0))
+        self.teConnState.setReadOnly(True)
 
-    def __show_popup_fail(self):
+    def __connection_GUI_setter(self, conn_status) -> None:
+        ''' Method to update client GUI, set it to connected or not connected view'''
+        if not conn_status:
+            self.teConnState.setTextColor(QColor(255, 0, 0))
+            self.teConnState.setText("Not connected")
+        else:
+            self.teConnState.setTextColor(QColor(0, 127, 0))
+            self.teConnState.setText("Connected")
 
-        msg = QMessageBox(self)
-        msg.setWindowTitle("Info")
-        msg.setText("Connection failed")
-        msg.setStandardButtons(QMessageBox.Retry | QMessageBox.Ok)
-        msg.setDefaultButton(QMessageBox.Retry)
-        msg.exec_()
+        self.lbSend.setEnabled(conn_status)
+        self.lbReceive.setEnabled(conn_status)
+        self.teSend.setEnabled(conn_status)
+        self.teReceive.setEnabled(conn_status)
+        self.btnSend.setEnabled(conn_status)
+
+    def __show_popup_fail(self, msg: str) -> None:
+
+        popup = QMessageBox(self)
+        popup.setWindowTitle("Info")
+        popup.setText(msg)
+        popup.setStandardButtons(QMessageBox.Retry | QMessageBox.Ok)
+        popup.setDefaultButton(QMessageBox.Retry)
+        popup.exec_()
 
     def __show_popup_success(self):
 
@@ -116,9 +147,16 @@ class GUI(QMainWindow):
         msg.setDefaultButton(QMessageBox.Ok)
         msg.exec_()
 
-if __name__ == '__main__':
+    def __connect(self):
+        ip = self.teIP.toPlainText()
+        port = int(self.tePort.toPlainText())
+        msg = self.client.connect(server_ip=ip, server_port=port)
+        if msg is not None: self.__show_popup_fail(msg=msg)
+        else: self.__connection_GUI_setter(conn_status=True)
 
-    app = QtWidgets.QApplication(sys.argv)
-    gui = GUI()
-    gui.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = ClientGUI()
+    window.show()
     sys.exit(app.exec_())
